ae67897e55263d4d7baca91c945128fde0f3803e
Martin Perry <mperr01@gmail.com>
2022-12-07 18:51:38 -0500

    Add referential integrity

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_174601) do
-  create_table "materials", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_221_207_180_842) do
+  create_table 'materials', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "materials_relationships", force: :cascade do |t|
-    t.integer "material_source_id", null: false
-    t.integer "material_dest_id", null: false
-    t.index "\"material_id\"", name: "index_materials_relationships_on_material_id_and_material_id"
+  create_table 'materials_relationships', force: :cascade do |t|
+    t.integer 'material_source_id', null: false
+    t.integer 'material_dest_id', null: false
+    t.index '"material_id"', name: 'index_materials_relationships_on_material_id_and_material_id'
   end
 
-  create_table "materials_relationships_relationships_types", id: false, force: :cascade do |t|
-    t.integer "materials_relationship_id", null: false
-    t.integer "materials_relationships_type_id", null: false
+  create_table 'materials_relationships_relationships_types', id: false, force: :cascade do |t|
+    t.integer 'materials_relationship_id', null: false
+    t.integer 'materials_relationships_type_id', null: false
   end
 
-  create_table "materials_relationships_types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'materials_relationships_types', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  add_foreign_key 'materials_relationships', 'materials', column: 'material_dest_id'
+  add_foreign_key 'materials_relationships', 'materials', column: 'material_source_id'
 end
